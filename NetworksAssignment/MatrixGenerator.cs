43f59715985afb129b40ca295a7a27bd50690e7d
August Kørvell <august@aggisoft.dk>
2017-10-25 12:46:33 +0200

    I made it all, f*** u mate

---
+++ NetworksAssignment/MatrixGenerator.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using MathNet.Numerics;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+using MathNet.Numerics.LinearAlgebra.Factorization;
+using System.IO;
+
+namespace NetworksAssignment
+{
+    class MatrixGenerator
+    {
+        private List<List<double>> matrix;
+        public Dictionary<string, int> nameMapping;
+
+        public MatrixGenerator()
+        {
+            matrix = new List<List<double>>();
+            nameMapping = new Dictionary<string, int>();
+
+            using (StreamReader reader = new StreamReader(File.OpenRead("friendships.txt")))
+            {
+                string line = "";
+                while ((line = reader.ReadLine()) != null)
+                {
+                    //if line contains user, check if user exists and add if not else continue
+                    if (line.Contains("user:"))
+                    {
+                        string name = line.Split(':').Last().Trim().ToLower();
+
+                        if (!nameMapping.Keys.Contains(name) && name != string.Empty)
+                        {
+                            matrix.Add(new List<double>());
+                            nameMapping.Add(name, matrix.Count - 1);
+                        }
+                    }
+                }
+            }
+
+            foreach (List<double> user in matrix)
+            {
+                for (int i = 0; i < matrix.Count; i++)
+                {
+                    user.Add(0);
+                }
+            }
+
+            using (StreamReader reader = new StreamReader(File.OpenRead("friendships.txt")))
+            {
+                string line = "";
+                while ((line = reader.ReadLine()) != null)
+                {
+                    //if line contains user, check if user exists and add if not else continue
+                    if (line.Contains("user:"))
+                    {
+                        string name = line.Split(':').Last().Trim().ToLower();
+                        line = reader.ReadLine();
+
+                        if (line.Contains("friends:"))
+                        {
+                            foreach (string friend in line.Split(':').Last().Split('\t'))
+                            {
+                                if(friend != string.Empty)
+                                    matrix[nameMapping[name]][nameMapping[friend.Trim().ToLower()]] = 1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+        public double[,] GetAdjancencyMatrix()
+        {
+            double[,] adjacencyMatrix = new double[matrix.Count, matrix.Count];
+
+            for (int i = 0; i < matrix.Count; i++)
+            {
+                for (int j = 0; j < matrix.Count; j++)
+                {
+                    adjacencyMatrix[i, j] = matrix[i][j];
+                }
+            }
+
+            return adjacencyMatrix;
+        }
+
+        public double[,] GetDegreeMatrix()
+        {
+            double[,] degreeMatrix = new double[matrix.Count, matrix.Count];
+
+            for (int i = 0; i < matrix.Count; i++)
+            {
+                for (int j = 0; j < matrix.Count; j++)
+                {
+                    degreeMatrix[i,j] = 0;
+                }
+            }
+
+            int userIndex = 0;
+            foreach (List<double> user in matrix)
+            {
+                foreach (double friend in user)
+                {
+                    degreeMatrix[userIndex, userIndex] += friend;
+                }
+
+                userIndex++;
+            }
+
+            return degreeMatrix;
+        }
+    }
+}
--- NetworksAssignment/Program.cs
+++ NetworksAssignment/Program.cs
@@ -15,29 +15,33 @@ namespace NetworksAssignment
     {
         static void Main(string[] args)
         {
-            //Matrix<double> AdjacencyArray = DenseMatrix.OfArray(new double[,] {
-            //    {0,1,1,0,0,0},
-            //    {1,0,1,0,0,0},
-            //    {1,1,0,1,0,0},
-            //    {0,0,1,0,1,1},
-            //    {0,0,0,1,0,1},
-            //    {0,0,0,1,1,0}
-            //});
-            //Matrix<double> DegreeArray = DenseMatrix.OfArray(new double[,]
-            //{
-            //    {2,0,0,0,0,0},
-            //    {0,2,0,0,0,0},
-            //    {0,0,3,0,0,0},
-            //    {0,0,0,3,0,0},
-            //    {0,0,0,0,2,0},
-            //    {0,0,0,0,0,2}
-            //});
-            //var unnormalizedlaplace = DegreeArray - AdjacencyArray;
-            //Console.WriteLine(unnormalizedlaplace);
-            //var evdmat = unnormalizedlaplace.Evd();
-            //Console.WriteLine(evdmat.EigenVectors);
-            //Console.WriteLine(evdmat.EigenValues);
+            MatrixGenerator mg = new MatrixGenerator();
+            List<Tuple<int, double>> eigenVectorMapping = new List<Tuple<int, double>>();
+            
+            Matrix<double> AdjacencyArray = DenseMatrix.OfArray(mg.GetAdjancencyMatrix());
+            Matrix<double> DegreeArray = DenseMatrix.OfArray(mg.GetDegreeMatrix());
 
+            Control.UseNativeMKL();
+            var unnormalizedlaplace = DegreeArray - AdjacencyArray;
+            Console.WriteLine("unnormalized");
+            Console.WriteLine(unnormalizedlaplace);
+            var evdmat = unnormalizedlaplace.Evd();
+            Console.WriteLine("Eigenvectors");
+            Console.WriteLine(evdmat.EigenVectors);
+
+            Console.WriteLine("Result:");
+
+            for (int i = 0; i < evdmat.EigenVectors.RowCount; i++)
+            {
+                eigenVectorMapping.Add(new Tuple<int, double>(i, evdmat.EigenVectors[i, 1]));
+            }
+
+            eigenVectorMapping = eigenVectorMapping.OrderBy(t => t.Item2).ToList();
+
+            foreach (Tuple<int, double> value in eigenVectorMapping)
+            {
+                Console.WriteLine(value.Item1 + ": " + value.Item2);
+            }
 
 
             Console.ReadLine();

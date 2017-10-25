c5505472808d906ec5c615294a4d91cba5994d3a
Reffotsirk Nged <kristoffer@hylleby.dk>
2017-10-25 09:42:44 +0200

    first stuff

---
+++ NetworksAssignment/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using MathNet.Numerics;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+using MathNet.Numerics.LinearAlgebra.Factorization;
+
+namespace NetworksAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Matrix<double> AdjacencyArray = DenseMatrix.OfArray(new double[,] {
+            //    {0,1,1,0,0,0},
+            //    {1,0,1,0,0,0},
+            //    {1,1,0,1,0,0},
+            //    {0,0,1,0,1,1},
+            //    {0,0,0,1,0,1},
+            //    {0,0,0,1,1,0}
+            //});
+            //Matrix<double> DegreeArray = DenseMatrix.OfArray(new double[,]
+            //{
+            //    {2,0,0,0,0,0},
+            //    {0,2,0,0,0,0},
+            //    {0,0,3,0,0,0},
+            //    {0,0,0,3,0,0},
+            //    {0,0,0,0,2,0},
+            //    {0,0,0,0,0,2}
+            //});
+            //var unnormalizedlaplace = DegreeArray - AdjacencyArray;
+            //Console.WriteLine(unnormalizedlaplace);
+            //var evdmat = unnormalizedlaplace.Evd();
+            //Console.WriteLine(evdmat.EigenVectors);
+            //Console.WriteLine(evdmat.EigenValues);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}

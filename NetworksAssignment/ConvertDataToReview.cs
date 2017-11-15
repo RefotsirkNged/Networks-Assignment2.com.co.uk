06dd92420f8fa27c7afc0828609dce79f7cfd3be
Reffotsirk Nged <kristoffer@hylleby.dk>
2017-11-15 12:10:59 +0100

    friendshipreviews

--- NetworksAssignment/ConvertDataToReview.cs
+++ NetworksAssignment/ConvertDataToReview.cs
@@ -3,12 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NetworksAssignment;
 
 namespace ConsoleApplication1
 {
     class ConvertDataToReview
     {
         public static List<Review> convertSentimentDataToReview(List<ReadSentimentTrainingData.SentimentDataType> dataList)
+        {
+            List<Review> newReviewList = new List<Review>();
+
+            Parallel.ForEach(dataList, currentReview =>
+            {
+                newReviewList.Add(currentReview.getReview());
+            });
+
+            //foreach(ReadSentimentTrainingData.SentimentDataType element in dataList)
+            //{
+            //    newReviewList.Add(element.getReview());
+            //}
+
+            return newReviewList;
+        }
+
+        public static List<Review> convertFriendshipReviewDataToReview(List<ReadFriendshipReviewData.FriendshipReviewDataType> dataList)
         {
             List<Review> newReviewList = new List<Review>();
 
---
+++ NetworksAssignment/ReadFriendshipReviewData.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApplication1;
+
+namespace NetworksAssignment
+{
+    class ReadFriendshipReviewData
+    {
+        public class FriendshipReviewDataType
+        {
+            public string productID;
+            public string userID;
+            public string profileName;
+            public float helpfullness;
+            public float score;
+            public int time;
+            public string summary;
+            public string review;
+            public List<string> friendsList;
+
+            public Review getReview()
+            {
+                Review temp = new Review();
+                temp.user = userID;
+                temp.review = review;
+                temp.tokens = Tokenizer.SplitString(review);
+                return temp;
+            }
+        }
+
+        public static List<Review> readFileAsReview(string filePath)
+        {
+            return ConvertDataToReview.convertFriendshipReviewDataToReview(readFile(filePath));
+        }
+
+        public static List<FriendshipReviewDataType> readFile(string filePath)
+        {
+            try
+            {
+                List<FriendshipReviewDataType> dataList = new List<FriendshipReviewDataType>();
+                // Open the text file using a stream reader.
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    string lineRead = sr.ReadLine();
+                    while (lineRead != null)
+                    {
+                        if (lineRead.Contains("user:"))
+                        {
+                            dataList.Add(new FriendshipReviewDataType());
+                            dataList.LastOrDefault().userID = lineRead.Split(':')[1];
+                        }
+
+                        else if (lineRead.Contains("friends:"))
+                            dataList.LastOrDefault().friendsList = lineRead.Split(':')[1].Split('\t').ToList();
+
+                        else if (lineRead.Contains("summary:"))
+                        {
+                            dataList.LastOrDefault().summary = lineRead.Split(':')[1];
+                        }
+                        else if (lineRead.Contains("review:"))
+                            dataList.LastOrDefault().review = lineRead.Split(':')[1];
+                        lineRead = sr.ReadLine();
+                    }
+                }
+                return dataList;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+    }
+}
